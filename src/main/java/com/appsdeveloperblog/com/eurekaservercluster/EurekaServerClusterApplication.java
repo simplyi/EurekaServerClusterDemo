f1e781355f564ff6a9f5dc8f750edb8338c0d80c
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2023-06-27 10:43:45 -0400

    Initial commit

---
+++ src/main/java/com/appsdeveloperblog/com/eurekaservercluster/EurekaServerClusterApplication.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.com.eurekaservercluster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerClusterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerClusterApplication.class, args);
+	}
+
+}
